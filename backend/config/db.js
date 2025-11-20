ae9c642804b1f3f2e6bbb727e47d18028d857578
riturajhacker <kumarrituraj2008@gmail.com>
2025-11-21 04:03:21 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/chatindia');
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/chat-india");
+    console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
+    process.exit(1);
   }
 };
 
