9733cc717aeea9c17a8b5f7f6038cdcbc82df7d2
riturajhacker <kumarrituraj2008@gmail.com>
2025-11-21 04:04:10 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true },
-  avatar: { type: String, default: "/logo.png" },
-  createdAt: { type: Date, default: Date.now }
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
